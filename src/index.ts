55a97f7d0016a7cd91693cf8b98b851988e2580f
norkator <martin.kankaanranta@hotmail.com>
2025-01-19 08:56:12 +0200

    llm bot actions for testing

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import WebSocket from 'ws';
 import * as dotenv from 'dotenv';
 import logger from './logger';
 import {ChatMessageInterface, StatusUpdateInterface, TableInterface} from './interfaces';
-import {fetchLLMChatCompletion} from './llm';
+import {fetchLLMChatCompletion, fetchLLMHoldemActionCompletion} from './llm';
 
 dotenv.config();
 
@@ -11,7 +11,7 @@ let playerId: number = -1;
 let playerName: string = '';
 let selectedTable: TableInterface | null = null;
 let playerCards: string[] = [];
-let middleCardsStr: string[] = [];
+let middleCards: string[] = [];
 
 // Handle connection open
 ws.on('open', () => {
@@ -45,16 +45,36 @@ ws.on('message', (data) => {
     case 'statusUpdate':
       const statusUpdate = message.data as StatusUpdateInterface;
       const isPlayerTurn = statusUpdate.playersData
-        .find(player => player.playerId === playerId)?.isPlayerTurn ?? false;
-      if (isPlayerTurn) {
+        .find(player => Number(player.playerId) === Number(playerId))?.isPlayerTurn ?? false;
+      middleCards = statusUpdate.middleCards ?? [];
+      if (isPlayerTurn && selectedTable !== null) {
         logger.info(`It's now our turn`);
+        fetchLLMHoldemActionCompletion(playerCards, middleCards).then((action: string | null) => {
+          if (action !== null) {
+            switch (action) {
+              case 'CALL':
+              case 'CHECK':
+                setCheck(selectedTable?.tableId || -1);
+                break;
+              // case 'RAISE':
+              //   const amount = ...
+              //   setRaise(table.tableId, amount);
+              default:
+                setFold(selectedTable?.tableId || -1);
+                break;
+            }
+          } else {
+            console.log('null action');
+            setFold(selectedTable?.tableId || -1)
+          }
+        });
       }
       break;
     case 'chatMessage':
       const chatMessage: ChatMessageInterface = message.data.chatMessage;
       if (selectedTable !== null && chatMessage.playerName !== playerName) {
         fetchLLMChatCompletion(
-          selectedTable.game, playerName, playerCards, middleCardsStr, chatMessage.playerName, chatMessage.message
+          selectedTable.game, playerName, playerCards, middleCards, chatMessage.playerName, chatMessage.message
         ).then((msg: string | null) => {
           if (msg !== null) {
             ws.send(JSON.stringify({key: 'chatMessage', message: msg}));
@@ -87,4 +107,20 @@ function findSuitableTable(tables: TableInterface[]): TableInterface | null {
   const filteredTables: TableInterface[] = tables
     .filter((t: TableInterface) => t.game === 'HOLDEM' && !t.passwordProtected && t.playerCount < t.maxSeats);
   return filteredTables.length > 0 ? filteredTables[0] : null
+}
+
+function setFold(tableId: number) {
+  const data = JSON.stringify({
+    key: 'setFold',
+    tableId: tableId,
+  });
+  ws.send(data);
+}
+
+function setCheck(tableId: number) {
+  const data = JSON.stringify({
+    key: 'setCheck',
+    tableId: tableId,
+  });
+  ws.send(data);
 }
--- src/llm.ts
+++ src/llm.ts
@@ -22,14 +22,14 @@ export async function fetchLLMChatCompletion(
   game: Game,
   playerName: string,
   playerCards: string[],
-  middleCardsStr: string[],
+  middleCards: string[],
   msgPlayerName: string,
   userMsg: string,
 ): Promise<string | null> {
 
   const gameInstruction = `You are a rude but humorous bot in a ${getGameName(game)} table and your name is ${playerName}`;
   const chatInstructions = `You are part of public chat where user called ${msgPlayerName} sent a message.`;
-  const cardsInstructions = `You have ${playerCards.join(', ')} cards and middle cards ${middleCardsStr.join(', ')} and you use this information for bluffing reasons.`;
+  const cardsInstructions = `You have ${playerCards.join(', ')} cards and middle cards ${middleCards.join(', ')} and you use this information for bluffing reasons.`;
   const limitations = `Keep answer under 40 characters.`
 
   const url = `${process.env.JAN_AI_SERVER_ADDRESS}/v1/chat/completions`;
@@ -37,6 +37,45 @@ export async function fetchLLMChatCompletion(
     messages: [
       {role: 'system', content: `${gameInstruction} ${chatInstructions} ${cardsInstructions} ${limitations}`},
       {role: 'user', content: userMsg},
+    ],
+    model: model,
+    stream: false,
+    max_tokens: 2048,
+    stop: null,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    temperature: 0.7,
+    top_p: 0.95,
+  };
+
+  try {
+    const response = await axios.post<ChatCompletionResponse>(url, data, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const {choices, usage} = response.data;
+    return choices.length > 0 ? choices[0].message.content : null;
+  } catch (error) {
+    return null;
+  }
+}
+
+
+export async function fetchLLMHoldemActionCompletion(
+  playerCards: string[],
+  middleCards: string[],
+): Promise<string | null> {
+
+  const gameInstruction = `You are playing in a Texas hold 'em table and your task is to determine next action to proceed.`;
+  const cardsInstructions = `You have ${playerCards.join(', ')} as hole cards and current middle cards are ${middleCards.join(', ')}.`;
+  const limitations = `You can only answer using one of these values: CALL, CHECK or FOLD.`
+
+  const url = `${process.env.JAN_AI_SERVER_ADDRESS}/v1/chat/completions`;
+  const data = {
+    messages: [
+      {role: 'system', content: `${gameInstruction} ${cardsInstructions} ${limitations}`},
+      {role: 'user', content: 'What is your action?'},
     ],
     model: model,
     stream: false,
